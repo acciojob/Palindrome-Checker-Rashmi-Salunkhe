533bee0bfad796534170ab9b3cad9daa1b855f12
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-09-02 21:43:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-// complete the given function
-
-function palindrome(str){
-
+function palindrome(str) {
+    // Remove all non-alphanumeric characters and convert to lowercase
+    const cleanedStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    
+    // Check if the cleaned string is the same forwards and backwards
+    return cleanedStr === cleanedStr.split('').reverse().join('');
 }
-module.exports = palindrome
+
+
+
+module.exports = palindrome;
